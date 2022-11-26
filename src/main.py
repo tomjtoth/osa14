cbc923df3072dc45d6ec2f8c68e16554846c2447
Tamás Tóth <tomjtoth@gmail.com>
2022-11-26 16:34:57 +0200

    about ready

--- src/main.py
+++ src/main.py
@@ -9,53 +9,70 @@ kello = pygame.time.Clock()
 font_hud = pygame.font.SysFont("Arial", 24)
 font_pop_up = pygame.font.SysFont("Arial", 34)
 
-yells = [
-    pygame.transform.rotate(font_pop_up.render(x, True, (255, 255, 255)), randint(-15,15)) \
-    for x in "PEW!,PSSHT!,BZZZZ!,ZAP!,KSSSH!,KFFF!,AAAARGH!!!!".split(",")
-]
+render = lambda s: pygame.transform.rotate(font_pop_up.render(s, True, (255, 255, 255)), randint(-15,15))
+
+yells = [render(x) for x in "PEW!,PSSHT!,BZZZZ!,ZAP!,KSSSH!,KFFF!,AAAARGH!!!!,BOO!,BOOHOO!!!".split(",")]
+
+main_menu_text = """Keys:
+
+W, A, S, D - up, down, left, right
+LSHIFT - run
+LMB, RMB - charge and fire left, right eyes
+
+R - restart / reset
+ESC - exit
+ENTER - new game
+
+Gameplay:
+
+1. get them coins
+2. exit through the door
+"""
 
 crossh = (0,0)
 
-
 def main_menu():
-    naytto.fill((0,0,0))
-    y = 10
-    for s in """
-    Keyboard shortcuts:
+    def redraw():
+        naytto.fill((0,0,0))
+        y = 10
+        for s in main_menu_text.split("\n"):
+            naytto.blit(font_hud.render(s,True, (255, 255, 255)), (10, y))
+            y += 24
 
-    W,A,S,D - up,down,left,right
-    LSHIFT - run
-    LMB,RMB - charge and fire left,right eyes
-    R - restart/reset
+        pygame.display.flip()
 
-    ESC - exit
-    ENTER - new game""".split("\n"):
-        naytto.blit(font_hud.render(s,True, (255, 255, 255)), (10, y))
-        y += 24
+    redraw()
 
-    pygame.display.flip()
-    
     while True:
         for ev in pygame.event.get():
-            if ev.type == pygame.QUIT \
-            or ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
+            if ev.type == pygame.QUIT:
                 exit()
-                
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_ESCAPE:
+                    exit()
+                if ev.key == pygame.K_RETURN:
+                    while new_game():
+                        pass # needed for reset/restart
+                    redraw()
 
         kello.tick(60)
 
+def select_difficulty():
+    pass
 
-def new_game():
+def new_game(difficulty = 2):
     
-    global player, ev_list, crossh, persistents
+    global player, ev_list, crossh, persistents, enemies, collectibles
 
     persistents = []
 
     player = Player()
 
+    door = Door()
+
     # spawn collectibles
-    collectibles = [Collectible() for _ in range(randint(10,20))]
-    enemies = [Enemy() for _ in range(randint(10,20))]
+    collectibles = [[Collectible(), PowerUp()][randint(0,1)] for _ in range(randint(10,20))]
+    enemies = [Enemy() for _ in range(randint(5,10))]
 
     Enemy.timeout = Enemy.original_timeout
 
@@ -86,10 +103,14 @@ def new_game():
         if Enemy.timeout > 0:
             Enemy.timeout -= 1
 
+        
         for enemy in enemies:
             enemy.update()
 
         player.update()
+
+        door.update()
+
 
         naytto.blit(font_hud.render(
             f"INTEGRITY: {player._health:.0f}, ENERGY: {player._energy:.0f}, POINTS: {player._score:.0f}", 
@@ -111,9 +132,9 @@ def new_game():
 
         # show floating text messages for a certain amount of frames
         for obj in persistents:
-            if obj[1] > 0:
-                naytto.blit(obj[0], (player._pos[0] + Player.width, player._pos[1] - 20)) 
-                obj[1] -= 1
+            if obj[2] > 0:
+                naytto.blit(obj[0], (obj[1]._pos[0] + obj[1].__class__.width, obj[1]._pos[1] - 20)) 
+                obj[2] -= 1
             else:
                 cleanup_persistents.append(obj)
         
@@ -126,10 +147,19 @@ def new_game():
 
         kello.tick(60)
 
+
+# Pythagoras + center points (?)
+dist_to_player = lambda x: math.sqrt(
+    math.pow((x._pos[0] + x.__class__.width/2) - (player._pos[0] + Player.width/2), 2) # x^2
+    + 
+    math.pow((x._pos[1] + x.__class__.height/2) - (player._pos[1] + Player.height/2), 2) # y^2
+)
+
 class Collectible:
-    spr = pygame.image.load("kolikko.png")
-    width = spr.get_width()
-    height = spr.get_height()
+    sprites = [pygame.image.load("kolikko.png")]
+    #sprites.append(pygame.)
+    width = sprites[0].get_width()
+    height = sprites[0].get_height()
 
     sx = naytto.get_width() - width
     sy = naytto.get_height() - height
@@ -141,16 +171,54 @@ class Collectible:
         self._collected = False
 
     def update(self):
-        #global naytto
+        if not self._collected:
+            self.__get_collected()
+            naytto.blit(Collectible.sprites[0], self._pos)
 
-        naytto.blit(Collectible.spr, self._pos)
+    def __get_collected(self):
+        if dist_to_player(self) < 50:
+            player._score += self._value
+            self._collected = True
 
 class PowerUp(Collectible):
     types = ['max_energy', 'max_integrity', 'energy', 'integrity']
 
     def __init__(self):
+        super().__init__()
         self._value = 10
-        self._type = PowerUp.types[randint(0,len(PowerUp.types)-1)]
+        self._type = PowerUp.types[randint(0, len(PowerUp.types) - 1)]
+
+class Door():
+    sprite = pygame.image.load("ovi.png")
+    width = sprite.get_width()
+    height = sprite.get_height()
+    sx = naytto.get_width() - width
+    sy = naytto.get_height() - height
+
+    def __init__(self):
+        self._enabled = False
+        self._pos = (randint(0, Door.sx), randint(0, Door.sy))
+
+    def update(self):
+        global enemies, collectibles
+
+        # disabled check..s
+        if False:
+            for enemy in enemies:
+                if enemy._health > 0:
+                    return
+            
+        for coll in collectibles:
+            if not coll._collected:
+                return
+        
+        if not self._enabled:
+            self._enabled = True
+        else:
+            naytto.blit(Door.sprite, self._pos)
+
+            if dist_to_player(self) < 50:
+                print("yaay, you won!")  
 
 class Weapon:
     def __init__(self, chargeable = False):
@@ -223,8 +291,8 @@ class Character:
         
 
     def _translate(self):
-        sx = Player.sx if self is Player else Enemy.sx
-        sy = Player.sy if self is Player else Enemy.sy
+        sx = Player.sx if isinstance(self, Player) else Enemy.sx
+        sy = Player.sy if isinstance(self, Player) else Enemy.sy
 
         px = self._pos[0]
         py = self._pos[1]
@@ -287,30 +355,23 @@ class Enemy(Character):
         self._pos = [randint(0, Enemy.sx), randint(0, Enemy.sy)]
 
     def update(self):
-        global naytto
+        #global naytto
 
-        self.__seek()
-        naytto.blit(Enemy.sprite, self._pos)
-    
-    def __attack(interval: int):
-        player._health -= 1
+        if self._health > 0:
+            self.__seek()
+            naytto.blit(Enemy.sprite, self._pos)
 
     def __seek(self):
-        
-        # Pythagoraan lause
-        distance = math.sqrt(
-            math.pow(self._pos[0] - player._pos[0], 2) # x^2
-            + 
-            math.pow(self._pos[1] - player._pos[1], 2) # y^2
-        )
-        
-        if distance > 200:
+        distance = dist_to_player(self)
+
+        if player._health == 0 or distance > 200:
             self.__idle()
-        elif distance > 15:
-            if Enemy.timeout == 0:
-                self.__close_in(self._speeds[0 if distance > 100 else 1])
+            
         else:
-            self.__attack()
+            if distance > 10:
+                self.__close_in(self._speeds[0 if distance > 100 else 1])
+            else:
+                player._health -= 1 # attack
 
     def __idle(self):
         dx = randint(-self._speeds[0], self._speeds[0])
@@ -323,7 +384,9 @@ class Enemy(Character):
             self._pos[1] += dy
 
     def __close_in(self, v: int):
-                
+        if Enemy.timeout != 0:
+            return
+
         # direction x
         if player._pos[0] > self._pos[0]:
             self._pos[0] += v
@@ -336,17 +399,43 @@ class Enemy(Character):
         else:
             self._pos[1] -= v
 
+    def got_shot(self):
+        global persistents, yells
+
+        self._health -= Eye.cost*player._damage_multiplier*2
+
+        if self._health <= 0:
+            txt = yells[randint(-2, -1)]
+            player._score += 50
+        else:
+            txt = render(f"{self._health}/{self._max_health}")
+
+
+        persistents.append([
+
+            # the pre-rendered text itself
+            txt,
+
+            # the text will be positioned in relation to this object:
+            self, 
+            
+            # n frames to show the text near the robot
+            15
+
+        ])
+     
 class Player(Character):
 
     # standing still
-    spr_mov = [pygame.image.load("robo.png")]
+    sprites = [[pygame.image.load("robo.png")]]
+    sprites[0].append(pygame.transform.rotate(sprites[0][0], -90))
     # walking
-    spr_mov.append([pygame.transform.rotate(spr_mov[0],-5), pygame.transform.rotate(spr_mov[0],5)])
+    sprites.append([pygame.transform.rotate(sprites[0][0], -5), pygame.transform.rotate(sprites[0][0], 5)])
     # running
-    spr_mov.append([pygame.transform.rotate(spr_mov[0],-10), pygame.transform.rotate(spr_mov[0],10)])
+    sprites.append([pygame.transform.rotate(sprites[0][0], -10), pygame.transform.rotate(sprites[0][0], 10)])
 
-    width = spr_mov[0].get_width()
-    height = spr_mov[0].get_height()
+    width = sprites[0][0].get_width()
+    height = sprites[0][0].get_height()
 
     sx = naytto.get_width() - width
     sy = naytto.get_height() - height
@@ -358,23 +447,31 @@ class Player(Character):
         self._energy_regen = 1
         self._score = 0
         self._pos = [randint(0, Player.sx), randint(0, Player.sy)]
+        self._damage_multiplier = 1
 
     def __translate(self):
         global naytto
         
-        state = super()._translate()
+        if self._health > 0:
+            state = super()._translate()
         
-        # standing still
-        if state == 0:
-            spr = Player.spr_mov[0]
+            # standing still
+            if state == 0:
+                spr = Player.sprites[0][0]
+
+            # walking
+            if state == 1:
+                spr = Player.sprites[1][self._mov_anim_tick//5 % 2]
+            
+            # running
+            elif state == 2:
+                spr = Player.sprites[2][self._mov_anim_tick//5 % 2]
         
-        # walking
-        elif state == 1:
-            spr = Player.spr_mov[1][self._mov_anim_tick//5 % 2]
+        else:
+            # dead
+            spr = Player.sprites[0][1]
         
-        # running
-        elif state == 2:
-            spr = Player.spr_mov[2][self._mov_anim_tick//5 % 2]
+      
 
         # draw the character
         naytto.blit(spr, (self._pos[0], self._pos[1]))
@@ -382,53 +479,58 @@ class Player(Character):
     def update(self):
         global ev_list
 
-        for ev in ev_list:
-            if ev.type == pygame.KEYDOWN:
-                if ev.key == pygame.K_w:
-                    self._up = True
-                if ev.key == pygame.K_s:
-                    self._down = True
-                if ev.key == pygame.K_a:
-                    self._left = True
-                if ev.key == pygame.K_d:
-                    self._right = True
-                if ev.key == pygame.K_LSHIFT:
-                    self._running = True
-                if ev.key == pygame.K_SPACE:
-                    self._dashing = True
-                
-            if ev.type == pygame.KEYUP:
-                if ev.key == pygame.K_w:
-                    self._up = False
-                if ev.key == pygame.K_s:
-                    self._down = False
-                if ev.key == pygame.K_a:
-                    self._left = False
-                if ev.key == pygame.K_d:
-                    self._right = False
-                if ev.key == pygame.K_LSHIFT:
-                    self._running = False
-
-            if ev.type == pygame.MOUSEBUTTONDOWN:
-                if ev.button == pygame.BUTTON_LEFT:
-                    self._eyes[0].charging = True
-
-                if ev.button == pygame.BUTTON_RIGHT:
-                    self._eyes[1].charging = True
-
-            if ev.type == pygame.MOUSEBUTTONUP:
-                if ev.button == pygame.BUTTON_LEFT:
-                    self._eyes[0].charging = False
+        if self._health > 0:
+            for ev in ev_list:
+                if ev.type == pygame.KEYDOWN:
+                    if ev.key == pygame.K_w:
+                        self._up = True
+                    if ev.key == pygame.K_s:
+                        self._down = True
+                    if ev.key == pygame.K_a:
+                        self._left = True
+                    if ev.key == pygame.K_d:
+                        self._right = True
+                    if ev.key == pygame.K_LSHIFT:
+                        self._running = True
+                    if ev.key == pygame.K_0:
+                        # CHEATER !!!
+                        global enemies, collectibles
+                        for c in collectibles:
+                            c._collected = True
+                        for e in enemies:
+                            e._health = 0
                     
-                if ev.button == pygame.BUTTON_RIGHT:
-                    self._eyes[1].charging = False
-        
+                if ev.type == pygame.KEYUP:
+                    if ev.key == pygame.K_w:
+                        self._up = False
+                    if ev.key == pygame.K_s:
+                        self._down = False
+                    if ev.key == pygame.K_a:
+                        self._left = False
+                    if ev.key == pygame.K_d:
+                        self._right = False
+                    if ev.key == pygame.K_LSHIFT:
+                        self._running = False
+
+                if ev.type == pygame.MOUSEBUTTONDOWN:
+                    if ev.button == pygame.BUTTON_LEFT:
+                        self._eyes[0].charging = True
+
+                    if ev.button == pygame.BUTTON_RIGHT:
+                        self._eyes[1].charging = True
+
+                if ev.type == pygame.MOUSEBUTTONUP:
+                    if ev.button == pygame.BUTTON_LEFT:
+                        self._eyes[0].charging = False
+                        
+                    if ev.button == pygame.BUTTON_RIGHT:
+                        self._eyes[1].charging = False
+            self.__manage_energy()
         
         self.__translate()
-        self.__manage_energy()
     
     def __shoot(self, eye: Eye):
-        global crossh, naytto, yells, persistents
+        global crossh, naytto, yells, persistents, enemies
         
         if self._running:
             pos = eye.pos[2][self._mov_anim_tick//5 % 2]
@@ -447,13 +549,23 @@ class Player(Character):
         # firing sound made visible
         persistents.append([
             
-            # the pre-rendered text itself
-            yells[randint(0, len(yells)-1)], 
+            # the pre-rendered text itself, except for AARGH which is preserved for ghosts
+            yells[randint(0, len(yells)-4)],
+
+            # the text will be positioned in relation to this object:
+            self, 
             
             # n frames to show the text near the robot
             15
 
         ])
+
+        for enemy in enemies:
+            if crossh[0] in range(enemy._pos[0], enemy._pos[0] + Enemy.width) \
+            and crossh[1] in range(enemy._pos[1], enemy._pos[1] + Enemy.height):
+                enemy.got_shot()
+                
+                break # 1 shot is for 1 ghost only, would be too easy otherwise...
         
     def __manage_energy(self):
         
@@ -486,8 +598,4 @@ class Player(Character):
                     eye.level -= 1
 
 
-while True:
-    main_menu()
-
-while new_game():
-    pass
+main_menu()
