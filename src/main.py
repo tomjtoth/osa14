a4efbed5c02b3c06d6c8bb303621b4fdc1931046
Tamás Tóth <tomjtoth@gmail.com>
2022-11-26 12:08:13 +0200

    main menu ~ implemented

--- src/main.py
+++ src/main.py
@@ -1,32 +1,146 @@
-# TODO:
-# make use of the rest of the sprites
-# haamut
-# kolikot
-# door
-# 
-# BONUS:
-# powerups to increase energy, increase/restore integrity
-# animate ghosts' movement
-# 
-
-# TEE PELI TÄHÄN
-import pygame
+import pygame, math
 from random import randint, randrange
 
+pygame.init()
+naytto = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption("Obviously Superb Application 14 (osa14)")
+pygame.mouse.set_visible(False)
+kello = pygame.time.Clock()
+font_hud = pygame.font.SysFont("Arial", 24)
+font_pop_up = pygame.font.SysFont("Arial", 34)
 
+yells = [
+    pygame.transform.rotate(font_pop_up.render(x, True, (255, 255, 255)), randint(-15,15)) \
+    for x in "PEW!,PSSHT!,BZZZZ!,ZAP!,KSSSH!,KFFF!,AAAARGH!!!!".split(",")
+]
+
+crossh = (0,0)
+
+
+def main_menu():
+    naytto.fill((0,0,0))
+    y = 10
+    for s in """
+    Keyboard shortcuts:
+
+    W,A,S,D - up,down,left,right
+    LSHIFT - run
+    LMB,RMB - charge and fire left,right eyes
+    R - restart/reset
+
+    ESC - exit
+    ENTER - new game""".split("\n"):
+        naytto.blit(font_hud.render(s,True, (255, 255, 255)), (10, y))
+        y += 24
+
+    pygame.display.flip()
+    
+    while True:
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT \
+            or ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
+                exit()
+                
+
+        kello.tick(60)
+
+
+def new_game():
+    
+    global player, ev_list, crossh, persistents
+
+    persistents = []
+
+    player = Player()
+
+    # spawn collectibles
+    collectibles = [Collectible() for _ in range(randint(10,20))]
+    enemies = [Enemy() for _ in range(randint(10,20))]
+
+    Enemy.timeout = Enemy.original_timeout
+
+    while True:
+        
+        # caching all the events
+        ev_list = pygame.event.get()
+        for ev in ev_list:
+            if ev.type == pygame.QUIT:
+                exit()
+
+            if ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
+                # back to main menu
+                return False
+
+            if ev.type == pygame.KEYDOWN and ev.key == pygame.K_r:
+                
+                return True
+
+            elif ev.type == pygame.MOUSEMOTION:
+                crossh = ev.pos
+
+        naytto.fill((40, 40, 40))
+        
+        for collectible in collectibles:
+            collectible.update()
+
+        if Enemy.timeout > 0:
+            Enemy.timeout -= 1
+
+        for enemy in enemies:
+            enemy.update()
+
+        player.update()
+
+        naytto.blit(font_hud.render(
+            f"INTEGRITY: {player._health:.0f}, ENERGY: {player._energy:.0f}, POINTS: {player._score:.0f}", 
+            True, (255, 255, 255)), (10, Player.sy + Player.height-20))
+
+
+        # draw crosshair
+        pygame.draw.line(naytto, (255 - player._eyes[0].level, 255, 255)
+            , (crossh[0]-20, crossh[1])
+            , (crossh[0]+20, crossh[1])
+            , 2)
+        
+        pygame.draw.line(naytto, (255 - player._eyes[1].level, 255, 255)
+            , (crossh[0], crossh[1]-20)
+            , (crossh[0], crossh[1]+20)
+            , 2)
+
+        cleanup_persistents = []
+
+        # show floating text messages for a certain amount of frames
+        for obj in persistents:
+            if obj[1] > 0:
+                naytto.blit(obj[0], (player._pos[0] + Player.width, player._pos[1] - 20)) 
+                obj[1] -= 1
+            else:
+                cleanup_persistents.append(obj)
+        
+        # remove above texts
+        for obj in cleanup_persistents:
+            persistents.remove(obj)
+
+        
+        pygame.display.flip()
+
+        kello.tick(60)
 
 class Collectible:
     spr = pygame.image.load("kolikko.png")
     width = spr.get_width()
     height = spr.get_height()
 
+    sx = naytto.get_width() - width
+    sy = naytto.get_height() - height
+
     def __init__(self):
         global sx, sy
         self._value = 1
-        self._pos = (randint(0,sx-1), randint(0,sy-1))
+        self._pos = (randint(0, Collectible.sx-1), randint(0, Collectible.sy-1))
         self._collected = False
 
-    def redraw(self):
+    def update(self):
         #global naytto
 
         naytto.blit(Collectible.spr, self._pos)
@@ -106,10 +220,11 @@ class Character:
             3    # running
         ]
         self._mov_anim_tick = 0
-        self._pos = [randint(0, sx), randint(0, sy)]
+        
 
     def _translate(self):
-        global sx, sy
+        sx = Player.sx if self is Player else Enemy.sx
+        sy = Player.sy if self is Player else Enemy.sy
 
         px = self._pos[0]
         py = self._pos[1]
@@ -155,20 +270,71 @@ class Character:
   
 class Enemy(Character):
 
-    # standing still
-    spr_mov = [[pygame.image.load("hirvio.png")]]
-    spr_mov[0].append(pygame.transform.flip(spr_mov[0][0], True, False))
-    # walking
-    spr_mov.append([pygame.transform.rotate(spr_mov[0][0],-5), pygame.transform.rotate(spr_mov[0][0],5)])
-    spr_mov.append([pygame.transform.rotate(spr_mov[0][1],-5), pygame.transform.rotate(spr_mov[0][1],5)])
-    # running
-    spr_mov.append([pygame.transform.rotate(spr_mov[0][0],-10), pygame.transform.rotate(spr_mov[0][0],10)])
-    spr_mov.append([pygame.transform.rotate(spr_mov[0][1],-10), pygame.transform.rotate(spr_mov[0][1],10)])
+    global naytto
 
-    def handle_events(self):
+    original_timeout = timeout = 120
+
+    # standing still
+    sprite = pygame.image.load("hirvio.png")
+    
+    width = sprite.get_width()
+    height = sprite.get_height()
+    sx = naytto.get_width() - width 
+    sy = naytto.get_height() - height 
+
+    def __init__(self):
+        super().__init__()
+        self._pos = [randint(0, Enemy.sx), randint(0, Enemy.sy)]
+
+    def update(self):
         global naytto
 
-        naytto.blit(Enemy.spr_mov[0][0], self._pos)
+        self.__seek()
+        naytto.blit(Enemy.sprite, self._pos)
+    
+    def __attack(interval: int):
+        player._health -= 1
+
+    def __seek(self):
+        
+        # Pythagoraan lause
+        distance = math.sqrt(
+            math.pow(self._pos[0] - player._pos[0], 2) # x^2
+            + 
+            math.pow(self._pos[1] - player._pos[1], 2) # y^2
+        )
+        
+        if distance > 200:
+            self.__idle()
+        elif distance > 15:
+            if Enemy.timeout == 0:
+                self.__close_in(self._speeds[0 if distance > 100 else 1])
+        else:
+            self.__attack()
+
+    def __idle(self):
+        dx = randint(-self._speeds[0], self._speeds[0])
+        dy = randint(-self._speeds[0], self._speeds[0])
+
+        if self._pos[0] + dx in range(0, Enemy.sx):
+            self._pos[0] += dx
+
+        if self._pos[1] + dy in range(0, Enemy.sy):
+            self._pos[1] += dy
+
+    def __close_in(self, v: int):
+                
+        # direction x
+        if player._pos[0] > self._pos[0]:
+            self._pos[0] += v
+        else:
+            self._pos[0] -= v
+
+        # direction y
+        if player._pos[1] > self._pos[1]:
+            self._pos[1] += v
+        else:
+            self._pos[1] -= v
 
 class Player(Character):
 
@@ -182,10 +348,16 @@ class Player(Character):
     width = spr_mov[0].get_width()
     height = spr_mov[0].get_height()
 
+    sx = naytto.get_width() - width
+    sy = naytto.get_height() - height
+
+
     def __init__(self):
         super().__init__()
         self._eyes = [Eye(0), Eye(1)]
         self._energy_regen = 1
+        self._score = 0
+        self._pos = [randint(0, Player.sx), randint(0, Player.sy)]
 
     def __translate(self):
         global naytto
@@ -207,7 +379,7 @@ class Player(Character):
         # draw the character
         naytto.blit(spr, (self._pos[0], self._pos[1]))
 
-    def handle_events(self):
+    def update(self):
         global ev_list
 
         for ev in ev_list:
@@ -256,7 +428,7 @@ class Player(Character):
         self.__manage_energy()
     
     def __shoot(self, eye: Eye):
-        global crossh, naytto, float_txt, persistents
+        global crossh, naytto, yells, persistents
         
         if self._running:
             pos = eye.pos[2][self._mov_anim_tick//5 % 2]
@@ -276,7 +448,7 @@ class Player(Character):
         persistents.append([
             
             # the pre-rendered text itself
-            float_txt[randint(0, len(float_txt)-1)], 
+            yells[randint(0, len(yells)-1)], 
             
             # n frames to show the text near the robot
             15
@@ -314,88 +486,8 @@ class Player(Character):
                     eye.level -= 1
 
 
-
-pygame.init()
-naytto = pygame.display.set_mode((1024, 768))
-pygame.display.set_caption("unambitious osa14 yritys")
-pygame.mouse.set_visible(False)
-kello = pygame.time.Clock()
-font_hud = pygame.font.SysFont("Arial", 24)
-font_pop_up = pygame.font.SysFont("Arial", 34)
-
-# näitä vilautetaan kun ammutaan
-float_txt = [
-    pygame.transform.rotate(font_pop_up.render(x, True, (255, 255, 255)), randint(-15,15)) \
-    for x in "PEW!,PSSHT!,BZZZZ!,ZAP!,KSSSH!,KFFF!,AAAARGH!!!!".split(",")
-]
-persistents = []
-crossh = (0,0)
-
-sx = naytto.get_width() - Player.width
-sy = naytto.get_height() - Player.height
-
-player = Player()
-
-# spawn collectibles
-collectibles = [Collectible() for _ in range(randint(10,20))]
-enemies = [Enemy() for _ in range(randint(10,20))]
-
 while True:
-    
-    # caching all the events
-    ev_list = pygame.event.get()
-    for ev in ev_list:
-        if ev.type == pygame.QUIT \
-        or ev.type == pygame.KEYDOWN and ev.key == pygame.K_ESCAPE:
-                exit()
+    main_menu()
 
-        elif ev.type == pygame.MOUSEMOTION:
-            crossh = ev.pos
-
-
-    naytto.fill((40, 40, 40))
-    
-    
-    for collectible in collectibles:
-        collectible.redraw()
-
-    for enemy in enemies:
-        enemy.handle_events()
-    
-    player.handle_events()
-    
-
-    naytto.blit(font_hud.render(
-        f"INTEGRITY: {player._health:.0f}, ENERGY: {player._energy:.0f}, POINTS: x", 
-        True, (255, 255, 255)), (10, sy+Player.height-20))
-
-
-    # draw crosshair
-    pygame.draw.line(naytto, (255 - player._eyes[0].level, 255, 255)
-        , (crossh[0]-20, crossh[1])
-        , (crossh[0]+20, crossh[1])
-        , 2)
-    
-    pygame.draw.line(naytto, (255 - player._eyes[1].level, 255, 255)
-        , (crossh[0], crossh[1]-20)
-        , (crossh[0], crossh[1]+20)
-        , 2)
-
-    cleanup_persistents = []
-
-    # show floating text messages for a certain amount of frames
-    for obj in persistents:
-        if obj[1] > 0:
-            naytto.blit(obj[0], (player._pos[0] + Player.width, player._pos[1] - 20)) 
-            obj[1] -= 1
-        else:
-            cleanup_persistents.append(obj)
-    
-    # remove above texts
-    for obj in cleanup_persistents:
-        persistents.remove(obj)
-
-    
-    pygame.display.flip()
-
-    kello.tick(60)
+while new_game():
+    pass
